b332dbacba19ca086dde22ce615e41539205fd27
YehonatanHen <yonatan.h333@gmail.com>
2021-05-14 21:00:26 +0300

    Implement the conversion to binary values

--- ConvertionFunctions/ColorImageToRGBArray.py
+++ ConvertionFunctions/ColorImageToRGBArray.py
@@ -1,7 +1,16 @@
 import numpy as np
 from PIL import Image
+import sys
+
+#To see the full output uncommit the line below
+np.set_printoptions(threshold=sys.maxsize)
 
 def RGBConvert(path):
+    #Open file and convert to RGB
     img = Image.open(path)
-    arr = np.array(img)  # 640x480x4 array
-    print(arr)
+    arr = np.array(img)
+
+    binArr = np.unpackbits(arr,axis=2)
+
+
+    print(binArr)
