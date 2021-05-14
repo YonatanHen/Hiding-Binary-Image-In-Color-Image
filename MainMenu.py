477dee16beaa1f37474874ed914e2f4e0a60e9ee
YehonatanHen <yonatan.h333@gmail.com>
2021-05-14 14:57:28 +0300

    Aligned text to center

--- MainMenu.py
+++ MainMenu.py
@@ -1,8 +1,9 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 
 
 class MainMenu(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainMenu, self).__init__()  # Call the inherited classes __init__ method
         uic.loadUi('dataSecurity.ui', self)  # Load the .ui file
+        self.Title.setAlignment(QtCore.Qt.AlignCenter)
         self.show()  # Show the GUI
