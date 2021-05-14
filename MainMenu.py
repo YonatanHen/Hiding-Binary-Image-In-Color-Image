f03182dd457ea9ba76fa8a299789294d5482c131
YehonatanHen <yonatan.h333@gmail.com>
2021-05-15 02:13:14 +0300

    Binary img UI input added and converted to binary

--- MainMenu.py
+++ MainMenu.py
@@ -1,29 +1,43 @@
 from PyQt5 import QtWidgets, uic, QtCore
 from PyQt5.QtCore import QDir, QFile, QFileInfo, QIODevice
 from PyQt5.QtWidgets import QVBoxLayout, QFileDialog
-from ConvertionFunctions.ColorImageToRGBArray import RGBConvert
+from functions.ColorImageToRGBArray import RGBConvert
+
 
 class MainMenu(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainMenu, self).__init__()  # Call the inherited classes __init__ method
         uic.loadUi('dataSecurity.ui', self)  # Load the .ui file
 
-        #Organize the title alignment
+        # define class variables
+
+        # Organize the title alignment
         self.Title.setAlignment(QtCore.Qt.AlignCenter)
 
-        #upload an image
         vbox = QVBoxLayout(self)
+        # upload color image
         vbox.addWidget(self.ColorImageBtn)
-        self.ColorImageBtn.clicked.connect(self.load_image)
+        self.ColorImageBtn.clicked.connect(lambda:self.load_image('c'))
+
+        # upload color image
+        vbox.addWidget(self.BinaryImageBtn)
+        self.BinaryImageBtn.clicked.connect(lambda:self.load_image('b'))
+
         self.show()  # Show the GUI
 
-    def load_image(self):
+
+    def load_image(self, t):
         name = QFileDialog.getOpenFileName(self, 'Select Color Image', QDir.currentPath(),
-                                            "Image files (*.jpg, *.gif, *.png)")
+                                           "Image files (*.jpg, *.gif, *.png)")
         if name:
             path = name[0]
             print(path)
-            RGBConvert(path)
-
+            if t == 'b':
+                self.binaryImg = RGBConvert(path)
+                print(self.binaryImg)
+            else:
+                self.colorImg = RGBConvert(path)
+                print(self.colorImg)
             return path
+
 
---
+++ functions/ColorImageToRGBArray.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PIL import Image
+import sys
+
+#To see the full output uncommit the line below
+np.set_printoptions(threshold=sys.maxsize)
+
+def RGBConvert(path):
+    '''Opens an image file and convert it to RGB, then convert the RGB values to binary
+    For binary images, the values will be series of ones or zeroes, we can take the lsb because it represents
+    the whole bits of the pixel
+    PAY ATTENTION! COLOR IMAGE CONVERTED TO 24 bits WHEREAS BINARY IMAGE CONVERTED TO 16 BITS!'''
+
+    img = Image.open(path)
+    arr = np.array(img)
+
+    binArr = np.unpackbits(arr,axis=2)
+
+
+    # print(binArr)
+    return binArr
