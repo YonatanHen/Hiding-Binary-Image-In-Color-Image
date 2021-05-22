c54380c319acc7625e85c493861807fde74971ed
YehonatanHen <yonatan.h333@gmail.com>
2021-05-22 13:44:27 +0300

    Resize is working!

---
+++ Functions/resizeImage.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+
+def shrinkImage(imgPath):
+    img = Image.open(imgPath)
+    width, height = img.size
+    new_width = int(width / 2)
+    new_height = int(height / 2)
+    img.thumbnail((new_width, new_height), Image.ANTIALIAS)
+    img.save(imgPath, quality=100, optimize=True)
+
+    return
+
+
+def enlargeImage(imgPath):
+    img = Image.open(imgPath)
+    width, height = img.size
+    new_width = int(width * 2)
+    new_height = int(height * 2)
+    newImg =img.resize((new_width, new_height))
+    newImg.save(imgPath)
+    print(img)
+
+    return
