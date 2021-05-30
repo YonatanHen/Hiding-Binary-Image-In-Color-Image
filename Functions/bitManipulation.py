db76def817c0c87ff5fee5057b505e6d0b88b75d
Dave-Sama <davech1228@gmail.com>
2021-05-30 14:44:49 +0300

    HVFlip fixed

--- Functions/bitManipulation.py
+++ Functions/bitManipulation.py
@@ -1,8 +1,10 @@
 def HVFlip(binImage):
     # switching bits + replace even with odd rows
-    for i in range(len(binImage) - 1):
+    for i in range(len(binImage) - 1, 2):
         for j in range(len(binImage[i]) - 2):
             binImage[i][j], binImage[i][j + 1] = binImage[i][j + 1], binImage[i][j]
         binImage[i], binImage[i + 1] = binImage[i + 1], binImage[i]
 
     return binImage
+
+
