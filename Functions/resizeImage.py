5da55ce6ab3433db63137b30c5dcd5ae33cf50ad
YehonatanHen <yonatan.h333@gmail.com>
2021-06-25 09:29:08 +0300

    Deleted uneccessary parts, rename images names

--- Functions/resizeImage.py
+++ Functions/resizeImage.py
@@ -9,19 +9,5 @@ def reduceImage(imgPath):
     new_height = int(height / 1.5)
     newImg = img.resize((new_width, new_height), Image.BICUBIC)
     newImg.save(imgPath, quality=95, optimized=True)
-    print(img)
-
-    return
-
-
-def enlargeImage(imgPath):
-    """Function increase image height and width 1.5 times - i.e the function restores original values"""
-    img = Image.open(imgPath)
-    width, height = img.size
-    new_width = int(width * 1.5)
-    new_height = int(height * 1.5)
-    newImg = img.resize((new_width, new_height), Image.ANTIALIAS)
-    newImg.save(imgPath, quality=95)
-
 
     return
